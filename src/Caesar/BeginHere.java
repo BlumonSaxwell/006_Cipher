d6cdac8d55816e5fda5de23e9796866cdf289b06
compsci <compsci@compsci>
2017-10-20 10:47:07 -0600

    Working except for the z wrap

--- src/Caesar/BeginHere.java
+++ src/Caesar/BeginHere.java
@@ -1,5 +1,23 @@
 package Caesar;
 
+import java.util.Scanner;
+
 public class BeginHere {
+
+
+	public static void main(String[] args) {
+		Scanner kbd = new Scanner(System.in);
+		
+		CaesarCipher myCipher = new CaesarCipher();
+		
+		System.out.println("Please enter the plain text to encode");
+		String plainText = kbd.nextLine();
+		
+		System.out.println("Please enter the key:");
+		int key = kbd.nextInt();
+		
+		String codedText = myCipher.encode(plainText);
+		System.out.println("Your secret message is: " + codedText);
+	}
 
 }
--- src/Caesar/CaesarCipher.java
+++ src/Caesar/CaesarCipher.java
@@ -1,5 +1,45 @@
 package Caesar;
 
 public class CaesarCipher {
+	
+	private String alpha = "abcdefghijklmnopqrstuvwxyz";
 
+	public String encode(String plainText, int key) {
+		
+		String secretText = "";
+		int index;
+		String keyAdded = "";
+		
+		System.out.println("test, alpha length = " + alpha.length());
+		System.out.println("Test, First char is: " + alpha.charAt(0));
+		System.out.println("Test, last char is: " + alpha.charAt(alpha.length()-1));
+		
+		for (int i = 0; i < alpha.length()-1; i++) {
+			System.out.println(alpha.charAt(i));
+		}
+		
+		System.out.println("/n/n");
+		
+		for (int i=0; i < plainText.length() ; i++)
+			System.out.println(plainText.charAt(i));
+		/*
+		 * each har that i read from plainText i find the index in slpha
+		 * for example, read A look up index fo A get 0
+		 * read M look up index for M get 12
+		 * then, add key to that index, so A index 0 becomes 0+key
+		 * 
+		 */
+		for (int i=0; i<plainText.length();i++) {
+			
+			alpha.indexOf(plainText.charAt(i));
+			System.out.println("index = " + index);
+			
+			keyAdded = index + key;
+			
+			System.out.println("encoded is " + alpha.charAt(keyAdded));
+		}
+		
+		return secretText;
+	}
+		
 }
